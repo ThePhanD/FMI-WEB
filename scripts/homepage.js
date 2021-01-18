7f0d0229378a7fe5cb245a80d7224867de91f489
krisi2603 <43946111+krisi2603@users.noreply.github.com>
2021-01-19 01:00:57 +0200

    Add files via upload

---
+++ scripts/homepage.js
@@ -0,0 +1,19 @@
+const goToRegister = event => {
+    event.preventDefault();
+
+    window.location = 'register.html';
+}
+
+const goToLogin = event => {
+    event.preventDefault();
+
+    window.location = 'login.html';
+}
+
+(function() {
+    const registerButton = document.getElementById('register_button');
+    registerButton.addEventListener('click', goToRegister);
+
+    const loginButton = document.getElementById('login_button');
+    loginButton.addEventListener('click', goToLogin);
+})();
