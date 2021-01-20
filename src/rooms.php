2080858395f4d2f26b1f04fb931c0291c3a64f46
krisi2603 <43946111+krisi2603@users.noreply.github.com>
2021-01-21 01:04:58 +0200

    Add files via upload

--- src/rooms.php
+++ src/rooms.php
@@ -3,35 +3,62 @@
 
     class Room {
 
-        private $roomNumber;
-        private $capacity;
+        private $roomName;
+        private $rowNumber;
+        private $colNumber;
+        private $creator;
+        private $music;
+        private $places;
 
         private $db;
 
-        public function __construct($roomNumber, $capacity) {
+        public function __construct($roomName, $rowNumber, $colNumber) {
 
-            $this->roomNumber = $roomNumber;
-            $this->capacity = $capacity;
+            $this->roomName = $roomNumber;
+            $this->rowNumber = $rowNumber;
+            $this->colNumber = $colNumber;
 
             $this->db = new Db();
         }
 
-        public function getRoomNumber() {
-            return $this->roomNumber;
+        public function getRoomName() {
+            return $this->roomName;
+        }
+        public function getRowNumber() {
+            return $this->rowNumber;
+        }
+        public function getColNumber() {
+            return $this->colNumber;
+        }
+        public function getCreator() {
+            return $this->creator;
+        }
+        public function getMusic() {
+            return $this->music;
+        }
+        public function getPlaces() {
+            return $this->places;
+        }
+        public function setCreator($creator) {
+            $this->creator = $creator;
+        }
+        public function setPlaces($places) {
+            $this->places = $places;
+        }
+        public function setMusic($music) {
+            $this->music = $music;
         }
 
-        public function getCapacity() {
-            return $this->capacity;
-        }
-
-        public function createRoom($roomNumber, $capacity) 
+        public function createRoom($creator, $music, $places) 
         {
-            $query = $this->db->insertRoomQuery(["room_number" => $roomNumber, "capacity" => $capacity]);
+            $query = $this->db->insertRoomQuery(["room_name" => $this->roomName, "colNumber" => $this->colNumber, "rowNumber" => $this->rowNumber,
+                                                 "creator" => $creator, "music" => $music, "places" => $places]);
 
             if ($query["success"]) 
             {
-                $this->roomNumber = $roomNumber;
-                $this->capacity = $capacity;
+                $this->creator = $creator;
+                $this->music = $music;
+                $this->places = $places;
 
                 return $query;
             } 
@@ -42,7 +69,7 @@
 
         public function roomExists() 
         {
-            $query = $this->db->selectRoomQuery(["room_number" => $this->roomNumber]);
+            $query = $this->db->selectRoomQuery(["room_name" => $this->roomName]);
 
             if ($query["success"]) 
             {
