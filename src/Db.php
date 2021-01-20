6b108153978fa26e41b526c52f62016f67a57b1a
krisi2603 <43946111+krisi2603@users.noreply.github.com>
2021-01-20 15:47:37 +0200

    Add files via upload

--- src/Db.php
+++ src/Db.php
@@ -11,8 +11,6 @@ class Db {
 
     private $insertRoomStatement;
     private $selectRoomStatement;
-    private $selectCapacityOfRoomStatement;
-
 
     public function __construct() {
 	
@@ -44,8 +42,12 @@ class Db {
             $this->connection->exec($sql);
         
             $sql = "CREATE TABLE  IF NOT EXISTS rooms (
-                room_number int PRIMARY KEY,
-                capacity int NOT NULL
+                room_name VARCHAR(16) PRIMARY KEY,
+                rowNumber int NOT NULL,
+                colNumber int NOT NULL,
+                creator VARCHAR(32) NOT NULL,
+                music VARCHAR(64) NOT NULL,
+                places NOT NULL
             )";
             $this->connection->exec($sql);
             
@@ -69,14 +71,12 @@ class Db {
         $sql = "SELECT * FROM users WHERE userid=:userid";
         $this->selectUserByIdStatement =  $this->connection->prepare($sql);
 
-        $sql = "INSERT INTO rooms(room_number, capacity) VALUES (:room_number, :capacity)";
+        $sql = "INSERT INTO rooms(room_name, rowNumber, colNumber, creator, music, places) VALUES (:room_name, :rowNumber, :colNumber, :creator, :music, :places)";
         $this->insertRoomStatement =  $this->connection->prepare($sql);
 
-        $sql = "SELECT * FROM rooms WHERE room_number=:room_number";
+        $sql = "SELECT * FROM rooms WHERE room_name=:room_name";
         $this->selectRoomStatement =  $this->connection->prepare($sql);
 
-        $sql = "SELECT capacity FROM rooms WHERE room_number=:room_number";
-        $this->selectCapacityOfRoomStatement =  $this->connection->prepare($sql);
     }
 
     public function insertUserQuery($data) {
@@ -124,7 +124,7 @@ class Db {
 
     public function insertRoomQuery($data) {
         try {
-            // ["room_number" => "...", "capacity => "..."]
+            // ["room_name" => "...", "rolNumber => "..."]
             $this->insertRoomStatement->execute($data);
 
             return ["success" => true];
@@ -138,16 +138,6 @@ class Db {
             $this->selectRoomStatement->execute($data);
 
             return ["success" => true, "data" => $this->selectRoomStatement];
-        } catch(PDOException $e) {
-            return ["success" => false, "error" => $e->getMessage()];
-        }
-    }
-
-    public function selectCapacityOfRoomQuery($data) {
-        try {
-            $this->selectCapacityOfRoomStatement->execute($data);
-
-            return ["success" => true, "data" => $this->selectCapacityOfRoomStatement];
         } catch(PDOException $e) {
             return ["success" => false, "error" => $e->getMessage()];
         }
